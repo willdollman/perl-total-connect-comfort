7a4a1c60df81654e8c2ef8c7c0cb3ff53e2c81de
Will Dollman <will@dollman.org>
2014-05-28 22:10:43 +0100

    Break out server connection into sub, and check return value

--- Device/TotalConnectComfort.pm
+++ Device/TotalConnectComfort.pm
@@ -14,8 +14,10 @@ use LWP::UserAgent;
 use base qw( Exporter );
 our @EXPORT_OK = qw( new );
 
+my $DEBUG = 1;
+$\ = "\n";
+
 sub new {
-    my $self = shift;
     my $username = shift || croak "No username supplied";
     my $password = shift || croak "No password supplied";
     my $app_id   = shift || croak "No application id supplied";
@@ -24,50 +26,64 @@ sub new {
     my $test_input = 't/login_response';
 
     my $response_body;
-    unless ($is_test) {
-        print "Doing some magic stuff. Stand back.\n";
-        print "Username: $username\nPassword: $password\n";
+    if ($is_test) {
+        print "Using cached response" if $DEBUG;
 
-        my $host = 'https://rs.alarmnet.com';
-        #my $host = 'http://dollman.org';
-        my $path = '/TotalConnectComfort/WebAPI/api/Session';
-        my $url = URI->new($host . $path);
-
-        my $login_params = {
-            'Username' => $username,
-            'Password' => $password,
-            'ApplicationId' => $app_id,
-        };
-        my $login_json = to_json($login_params);
-
-        my $ua = LWP::UserAgent->new;
-        $ua->agent('User-Agent: RestSharp 104.1.0.0');
-
-        my $request = HTTP::Request->new(POST => $url);
-        $request->header('Content-Type' => 'application/json');
-        $request->content($login_json);
-        print "Request is ", Dumper $request;
-
-        my $r = $ua->request($request);
-        print "Response is ", $r->content;
-
-        # check response code + data is as expected
-
-        # save to file so we can reuse it
-        open(my $response_fh, '>', $test_input)
-            or die "Unable to save to file: $!";
-        print $response_fh $r->content;
-
-        $response_body = $r->content;
-    }
-    else {
         open(my $test_input_fh, '<', $test_input)
             or die "Unable to open test input file";
         $response_body = read_file($test_input_fh);
     }
+    else {
+        my $r = do_login($username, $password, $app_id);
+        # save to file so we can reuse it
+        open(my $response_fh, '>', $test_input)
+            or die "Unable to save to file: $!";
+        $response_body = $r->content;
+    }
 
-    print "Response body is:\n$response_body\n";
+    my $login_response = from_json($response_body);
+    die "Server response did not contain a session id token"
+        unless $login_response->{sessionId};
 
+    my $self;
+    $self->{sessionId} = $login_response->{sessionId};
+    $self->{username}  = $login_response->{userInfo}->{username};
+    # include a valid_until counter - unsure how long sessions are valid for
+    bless $self;
+}
+
+sub do_login {
+    my $username = shift || die "No username supplied";
+    my $password = shift || croak "No password supplied";
+    my $app_id   = shift || croak "No application id supplied";
+
+    print "Performing login on server" if $DEBUG;
+
+    my $host = 'https://rs.alarmnet.com';
+    my $path = '/TotalConnectComfort/WebAPI/api/Session';
+    my $url = URI->new($host . $path);
+
+    my $login_params = {
+        'Username' => $username,
+        'Password' => $password,
+        'ApplicationId' => $app_id,
+    };
+    my $login_json = to_json($login_params);
+
+    my $ua = LWP::UserAgent->new;
+    $ua->agent('User-Agent: RestSharp 104.1.0.0');
+
+    my $request = HTTP::Request->new(POST => $url);
+    $request->header('Content-Type' => 'application/json');
+    $request->content($login_json);
+
+    my $r = $ua->request($request);
+
+    die "Invalid username/password" if $r->code == '401';
+    die "App id is incorrect"       if $r->code == '400';
+    die "Unknown error occurred"    if $r->code != '200';
+
+    return $r;
 }
 
 1;
