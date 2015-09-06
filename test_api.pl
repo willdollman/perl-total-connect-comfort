febb426dd9c83b802d9c463500e60ee351fe15d5
Ed Wildgoose <git@wildgooses.com>
2015-09-06 22:04:16 +0100

    Update to use new API as of Sep 2015
    This means shifting to new login process
    New upstream url: https://tccna.honeywell.com
    API is split into functions to get devices and status separately
    Added new functions to *set* schedules, temp override and system status
    
    Lacks documentation, but implements nearly all functions used
    by the Android app for heating control (not tested hot water or cooling)

--- test_api.pl
+++ test_api.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use warnings;
 use strict;
@@ -13,23 +13,25 @@ my ($username, $password) = @ARGV;
 
 # Log in
 my $cn = Device::TotalConnectComfort->new( $username, $password );
-print "Logged in as $cn->{username}";
+print "Logged in\n";
+my $account = $cn->get_user_account;
 
 # Get data for all our locations
 my $locations_data = $cn->get_locations;
 describe_locations($locations_data);
 
 # Set default location id for other requests
-my $location_id = $locations_data->[0]->{locationID};
+my $location_id = $locations_data->[0]->{locationInfo}->{locationId};
 print "Setting default location ID to $location_id\n";
 
 # Get data on the default location
-my $location_data = $cn->get_location($location_id);
-describe_devices( $location_data->[0] );
+my $status_data = $cn->get_status($location_id);
+describe_status( $status_data );
 
 # Describe gateways
 my $gateway_data = $cn->get_gateways($location_id);
 describe_gateways($gateway_data);
+
 
 # Describe each location found
 sub describe_locations {
@@ -38,27 +40,26 @@ sub describe_locations {
     print "Found ", scalar @$locations_data, ' ',
       ( scalar @$locations_data == 1 ) ? 'location' : 'locations', "\n";
     for my $location (@$locations_data) {
-        print
-          "Location $location->{locationID} ($location->{streetAddress})\n---";
-        describe_devices($location);
+        print "Location $location->{locationInfo}->{locationId} ($location->{locationInfo}->{streetAddress})\n---";
+        # describe_devices($location);
     }
 }
 
-# Describe devices at a given location
-sub describe_devices {
-    my $location_data = shift;
+# Describe status at a given location
+sub describe_status {
+    my $status_data = shift;
 
-    #print "\n", scalar @{$location_data->{devices}}, " devices:";
+    #print "\n", scalar @{$status_data->{devices}}, " devices:";
     my $tb =
-      Text::Table->new( 'Location', 'Temperature', 'Status', 'Setpoint' );
-    for my $device ( @{ $location_data->{devices} } ) {
+      Text::Table->new( 'Location', 'Temperature °C', 'Status', 'Setpoint °C', 'ZoneId' );
+    for my $device ( @{ $status_data->{gateways}->[0]->{temperatureControlSystems}->[0]->{zones} } ) {
         $tb->load(
             [
-                "$device->{name} ",
-                "$device->{thermostat}->{indoorTemperature}°C",
-                $device->{thermostat}->{changeableValues}->{mode},
-                $device->{thermostat}->{changeableValues}->{heatSetpoint}
-                  ->{value},
+                $device->{name},
+                "$device->{temperatureStatus}->{temperature}",
+                $device->{setpointStatus}->{setpointMode},
+                $device->{setpointStatus}->{targetHeatTemperature},
+                $device->{zoneId},
             ]
         );
     }
@@ -71,6 +72,7 @@ sub describe_gateways {
     my $gateway_data = shift;
 
     for my $gw (@$gateway_data) {
-        print "Found gateway ID $gw->{gatewayID} at location $gw->{locationId}";
+        print "Found gateway ID: $gw->{gatewayId}, MAC address: $gw->{mac}";
     }
 }
+
